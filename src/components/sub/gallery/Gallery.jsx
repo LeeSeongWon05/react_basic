82d1bbde7fb4eaaec33f4e5565554e5ee9a626b5
LeeSeongWon05 <lsw050103@gmail.com>
2023-10-10 11:23:21 +0900

    겔러리 업그레이드

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -5,16 +5,29 @@ import Masonry from 'react-masonry-component';
 
 export default function Gallery() {
 	const [Pics, setPics] = useState([]);
+	const my_id = '199305274@N06';
 
-	const fetchData = async () => {
+	const fetchData = async (opt) => {
+		let url = '';
 		const api_key = '2a1a0aebb34012a99c23e13b49175343';
 		const method_interest = 'flickr.interestingness.getList';
-		const num = 50;
-		const url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
+		const method_user = 'flickr.people.getPhotos';
+		const method_search = 'flickr.photos.search';
 
-		//만약 특정함수가 promise를 반환한다면 warpping함수로 묶어준뒤 async 지정
-		//각각의 promise 반환 함수 앞쪽에 await를 붙이기만 하면 해당 코드는 동기화됨
-		//지저분하게 depth를 들여쓰기 해가면서 then구문을 호출할 필요가 없음
+		const num = 50;
+
+		//fetching함수 호출시 타입값이 있는 객체를 인수로 전달하면 해당 타입에 따라 호출 URL이 변경되고
+		//해당URL을 통해 받아지는 데이터로 달라짐
+		if (opt.type === 'interest') {
+			url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
+		}
+		if (opt.type === 'user') {
+			url = `https://www.flickr.com/services/rest/?method=${method_user}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json&user_id=${opt.id}`;
+		}
+		if (opt.type === 'search') {
+			url = `https://www.flickr.com/services/rest/?method=${method_search}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json&tags=${opt.tags}`;
+		}
+
 		const data = await fetch(url);
 		const json = await data.json();
 		console.log(json.photos.photo);
@@ -22,11 +35,19 @@ export default function Gallery() {
 	};
 
 	useEffect(() => {
-		fetchData();
+		//type: 'interest' 인터레스트 방식 갤러리 호출
+		//type: 'user' 사용자 아이디 계정의 갤러리 호출
+		//type: 'search' 검색키워드로 갤러리 호출
+		//fetchData({ type: 'user', id: my_id });
+		//fetchData({ type: 'interest' });
+		fetchData({ type: 'search', tags: 'ocean' });
 	}, []);
 
 	return (
 		<Layout title={'Gallery'}>
+			<button onClick={() => fetchData({ type: 'user', id: my_id })}>My Gallery</button>
+			<button onClick={() => fetchData({ type: 'interest' })}>Interest Gallery</button>
+
 			<div className='picFrame'>
 				<Masonry
 					elementType={'div'}
@@ -50,7 +71,9 @@ export default function Gallery() {
 											src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
 											alt={data.owner}
 										/>
-										<span>{data.owner}</span>
+										<span onClick={() => fetchData({ type: 'user', id: data.owner })}>
+											{data.owner}
+										</span>
 									</div>
 								</div>
 							</article>
