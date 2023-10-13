91f9525b5502cc339150b79df30bf9707486cd6c
LeeSeongWon05 <lsw050103@gmail.com>
2023-10-13 13:17:29 +0900

    삭제 기능 및 수정 기능 개선

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -5,7 +5,10 @@ import { useRef, useState } from 'react';
 export default function Community() {
 	const refInput = useRef(null);
 	const refTextarea = useRef(null);
+	const refEditInput = useRef(null);
+	const refEditTextarea = useRef(null);
 	const [Posts, setPosts] = useState([]);
+	const [Allowed, setAllowed] = useState(true);
 	console.log(Posts);
 
 	const resetForm = () => {
@@ -23,17 +26,49 @@ export default function Community() {
 	};
 
 	const deletePost = (delIndex) => {
-		//기존 Posts배열을 반복 돌면서 인수로 전달된 삭제 순번값과 현재 반복되는 배열의 순번값이 같지 않은 것만 리턴
-		setPosts(Posts.filter((_, idx) => delIndex !== idx));
+		if (window.confirm('정말 해당 게시글을 삭제하겠습니까?')) {
+			//기존 Posts배열을 반복 돌면서 인수로 전달된 삭제 순번값과 현재 반복되는 배열의 순번값이 같지 않은 것만 리턴
+			setPosts(Posts.filter((_, idx) => delIndex !== idx));
+		}
 	};
 
 	//해당 글을 수정모드로 변경시키는 함수
 	const enableUpdate = (editIndex) => {
+		//수정모드 함수 호출시 Allowed가 true가 아니면 return으로 함수 강제 종료
+		if (!Allowed) return;
+		//일단 수정모드에 진입하면 강제로 Allowed값을 false로 변경해서 다른 글 수정모드 진입금지 처리
+		setAllowed(false);
 		setPosts(
 			//Posts 배열값을 반복돌면서 인수로 전달된 수정할 포스트의 순번값과 현재 반복도는 배열의 포스트 순번값이 일치하면
 			//해당 글을 수정처리해야되므로 해당 객체에 enableUpdate=true값을 추가
 			Posts.map((post, idx) => {
 				if (editIndex === idx) post.enableUpdate = true;
+				return post;
+			})
+		);
+	};
+
+	//해당 글을 출력모드로 변경시키는 함수
+	const disableUpdate = (editIndex) => {
+		setAllowed(true);
+		setPosts(
+			Posts.map((post, idx) => {
+				if (editIndex === idx) post.enableUpdate = false;
+				return post;
+			})
+		);
+	};
+
+	//실제 글 수정하는 함수
+	const updatePost = (updateIndex) => {
+		//setPosts로 기존 Post배열같은 덮어쓰기해서 변경
+		//리액트에서는 참조형 자료는 무조건 배열값을 Deep copy한뒤 변경
+		setPosts(
+			Posts.map((post, idx) => {
+				if (updateIndex === idx) {
+					post.title = refEditInput.current.value;
+					post.content = refEditTextarea.current.value;
+				}
 				return post;
 			})
 		);
@@ -54,19 +89,49 @@ export default function Community() {
 
 			<div className='showBox'>
 				{Posts.map((post, idx) => {
-					return (
-						<article key={idx}>
-							<div className='txt'>
-								<h2>{post.title}</h2>
-								<p>{post.content}</p>
-							</div>
+					if (post.enableUpdate) {
+						//수정 모드 렌더링
+						return (
+							<article key={idx}>
+								<div className='txt'>
+									<input type='text' defaultValue={post.title} ref={refEditInput} />
+									<br />
+									<textarea
+										//react에서 value속성을 적용하려면 무조건 onChange이벤트 연결 필수
+										//onChange이벤트 연결하지 않을때에는 value가닌 defaultValue속성 적용
+										defaultValue={post.content}
+										ref={refEditTextarea}
+									/>
+								</div>
+								<nav className='btnSet'>
+									<button onClick={() => disableUpdate(idx)}>Cancel</button>
+									<button
+										onClick={() => {
+											updatePost(idx);
+											disableUpdate(idx);
+										}}
+									>
+										Update
+									</button>
+								</nav>
+							</article>
+						);
+					} else {
+						//출력 모드 렌더링
+						return (
+							<article key={idx}>
+								<div className='txt'>
+									<h2>{post.title}</h2>
+									<p>{post.content}</p>
+								</div>
 
-							<nav className='btnSet'>
-								<button onClick={() => enableUpdate(idx)}>Edit</button>
-								<button onClick={() => deletePost(idx)}>Delete</button>
-							</nav>
-						</article>
-					);
+								<nav className='btnSet'>
+									<button onClick={() => enableUpdate(idx)}>Edit</button>
+									<button onClick={() => deletePost(idx)}>Delete</button>
+								</nav>
+							</article>
+						);
+					}
 				})}
 			</div>
 		</Layout>
